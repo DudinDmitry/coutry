7a5612917b190e33ca558438f377940e7a84e8ff
Dmitry <dimon.dudin1996@gmail.com>
2020-05-12 23:31:13 +0300

    Коррекция контроллеров

--- app/Http/Controllers/mainController.php
+++ app/Http/Controllers/mainController.php
@@ -12,26 +12,30 @@ class mainController extends Controller
     public function main()
     {
         $countries = Country::all();
-        foreach ($countries as $country) {
-            echo '<a href="city/'.$country->id.'">' . $country->title .' ('.
-                count($country->city).')</a><br>';
+                foreach ($countries as $country) {
+            echo '<a href="city/' . $country->id . '">' . $country->title . ' (' .
+                count($country->city) . ')</a><br>';
         }
     }
 
     public function city($id)
     {
-        $cities=Country::find($id)->city;
-        foreach ($cities as $city)
-        {
-            echo '<a href="attractions/'.$city->id.'">'.$city->title.' ('.count($city->attraction).')</a><br>';
+        $cities = Country::find($id)->city;
+        echo '<a href="/">Вернуться к странам</a><br><br>';
+        foreach ($cities as $city) {
+            echo '<a href="attractions/' . $city->id . '">' . $city->title . ' (' . count($city->attraction) . ')</a><br>';
         }
     }
+
     public function attraction($id)
     {
-        $attractions=City::find($id)->attraction;
-        foreach ($attractions as $attraction)
-        {
-            echo $attraction->title.'<br>';
+        $attractions = City::find($id)->attraction;
+        $id_country = City::find($id)->country->id;
+        echo '<a href="../' . $id_country . '">Вернуться к городам</a><br><br>';
+        foreach ($attractions as $attraction) {
+            echo $attraction->title . '<br>
+            Описание: '.$attraction->description.'<br><br>
+';
         }
     }
 }
